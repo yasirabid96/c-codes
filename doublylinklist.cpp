1d86e2b8fa0280bbc73e09c7897b10da1b43178e
yasirabid96 <57865896+yasirabid96@users.noreply.github.com>
2020-10-16 16:05:44 +0500

    circularlinkedlist.cpp

--- doublylinklist.cpp
+++ doublylinklist.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
-
+//circular linked list
 using namespace std;
-
 class node{
-	
 	int data;
 	node *next;
 	public:
@@ -12,51 +10,73 @@ class node{
 		void append();
 		void del();
 		void update();
-		
-		
 };
-
-
-node *current,*head,*tail,*temp,*c=NULL;
+node *head,*tail,*current,*c,*temp=NULL;
 int size=0;
-void node::insert()
-{
-	int val;
-	cout<<"Enter value to add"<<endl;
-	cin>>val;
-	temp = new node();
-	temp->next=NULL;
-	temp->data=val;
+void node::insert(){
+	int value;
+	cout<<"Enter Value to insert "<<endl;
+	cin>>value;
+	temp=new node();
+	temp->data=value;
+	temp->next=head;
 	size++;
 	if(head==NULL)
 	{
 		head=temp;
 		tail=temp;
 		current=temp;
-		c=temp;
 	}
-	else
+	else 
 	{
 		tail->next=temp;
 		tail=tail->next;
 		tail->next=head;
 	}
 }
-
-
-void node::append(){
+void node::show()
+{
+	if(size==0)
+	{
+		cout<<"NO DATA"<<endl;
+	}
+	else
+	{
 	
+	cout<<"All Data"<<endl;
+	current=head;
+	if(size==1)
+	{
+	cout<<"DATA :"<<current->data<<endl;
+	}
+	else
+	{
+	
+	while(current->next!=head)
+	{
+		cout<<"DATA :"<<current->data<<endl;
+		current=current->next;
+	}
+
+		cout<<"DATA :"<<current->data<<endl;
+	}
+}
+}
+void node::append(){
 	int pos;
 	int value;
-	cout<<"Enter position you want to append"<<endl;
+	cout<<"Enter psotion to append data"<<endl;
 	cin>>pos;
-	cout<<"Enter Value you want to add"<<endl;
+	cout<<"Enter value to add"<<endl;
 	cin>>value;
 	if(size==0)
 	{
-		temp=new node();
-		temp->data=value;
+		temp = new node();
 		temp->next=NULL;
+		temp->data=value;
+		head=temp;
+		tail=temp;
+		current=temp;
 		size++;
 	}
 	else if(pos==1&&pos<size)
@@ -65,64 +85,50 @@ void node::append(){
 		temp->data=value;
 		temp->next=head;
 		head=temp;
+		tail->next=head;
 		size++;
 	}
 	else if(pos>1&&pos<size)
 	{
-		current=head;
 		c=head;
-		for(int i=0;i<pos;i++)
+		current=head;
+		for(int i=1;i<pos;i++)
 		{
 			c=current;
 			current=current->next;
 		}
 		temp=new node();
+		temp->data=value;
 		c->next=temp;
 		temp->next=current;
-		temp->data=value;
 		size++;
 	}
 	else if(pos==size)
 	{
-		c=head;
-		current=head;
-		for(int i=1;i<size;i++)
-		{
-			c=current;
-			current=current->next;
-//			cout<<current->data<<endl;
-		}
 		temp=new node();
 		temp->data=value;
-		current->next=temp;
-		tail=temp;
-		temp->next=head;
+		tail->next=temp;
+		tail=tail->next;
+		tail->next=head;
 		size++;
-//		cout<<temp->next->data;
+		
 	}
-	
 }
-
-
-void node::del()
-{
-		int pos;
-	cout<<"Enter position you want to delete"<<endl;
+void node::del(){
+	int pos;
+	cout<<"Enter psotion to delete data"<<endl;
 	cin>>pos;
-	if(size==0)
+	if(head==NULL)
 	{
 		cout<<"No data to delete"<<endl;
-		
 	}
 	else if(pos==1&&pos==size)
 	{
-			current=head;
-			delete current;
-			head=NULL;
-			tail=NULL;
-			temp=NULL;
-			c=NULL;
-		//	cout<<head->data;
+		current=head;
+		delete current;
+		head=NULL;
+		tail=NULL;
+		current=NULL;
 		size--;
 	}
 	else if(pos==1&&pos<size)
@@ -130,17 +136,15 @@ void node::del()
 		current=head;
 		head=head->next;
 		tail->next=head;
-		cout<<head->data<<current->data;
 		delete current;
 		size--;
 	}
 	else if(pos>1&&pos<size)
 	{
-			current=head;
-			c=head;
+		c=head;
+		current=head;
 		for(int i=1;i<pos;i++)
 		{
-			
 			c=current;
 			current=current->next;
 		}
@@ -150,84 +154,53 @@ void node::del()
 	}
 	else if(pos==size)
 	{
-			c=head;
+		c=head;
 		current=head;
 		for(int i=1;i<size;i++)
 		{
 			c=current;
 			current=current->next;
-//			cout<<current->data<<endl;
 		}
 		tail=c;
-		c->next=head;
+		tail->next=head;
 		delete current;
 		size--;
-		
 	}
 }
-
-
-void node::update()
-{
+void node::update(){
 	
 }
-
-void node::show()
-{
-	cout<<"ALL DATA"<<endl;
-	current=head;
-	if(head==NULL)
-	{
-		cout<<"No data to show"<<endl;
-	}
-	else
-	{
-	
-	while(current->next!=head)
-	{
-		cout<<current->data<<endl;
-		current=current->next;
-	}
-	cout<<current->data<<endl;
-}
-}
-
-
 
 int main()
 {
-	node n;
 	int a;
-	
-while(1)
-{
-	
-	cout<<"Press 1 to add"<<endl;
-	cout<<"Press 2 to show"<<endl;
-	cout<<"Press 3 to append"<<endl;
-	cout<<"Press 4 to delete"<<endl;
-	cout<<"Press 5 to update"<<endl;
-	cin>>a;
-	switch(a)
+	node n;
+	while(1)
 	{
-		case 1:
-			n.insert();
-			break;
-		case 2:
-			n.show();
-			break;
-		case 3:
-			n.append();
-			break;
-		case 4:
-			n.del();
-			break;
-		case 5:
-			n.update();
-			break;
-						
+		cout<<"Press 1 to add data"<<endl;
+		cout<<"Press 2 to display data"<<endl;
+		cout<<"Press 3 to append data"<<endl;
+		cout<<"Press 4 to delete data"<<endl;
+		cout<<"Press 5 to update data"<<endl;
+		cin>>a;
+		switch(a)
+		{
+			case 1:
+				n.insert();
+				break;
+			case 2:
+				n.show();
+				break;
+			case 3:
+				n.append();
+				break;
+			case 4:
+				n.del();
+				break;
+			case 5:
+				n.update();
+				break;				
+		}
 	}
 }
-}
-
 
